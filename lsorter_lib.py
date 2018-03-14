ebd49fe849e801e855e252aee5a74f2659c8205e
bb1950328 <bb1950328@gmail.com>
2018-03-14 18:06:18 +0100

    Update lsorter_lib.py

--- lsorter_lib.py
+++ lsorter_lib.py
@@ -1,12 +1,20 @@
+import pigpio, time, sys, os
+
+pi = pigpio.pi()
+
 class slideturner():
-    def __init__(self, num_boxes, min_servo=675, max_servo=2350):
+    def __init__(self, num_boxes, min_servo=675, max_servo=2350, pin=4):
         self.num_boxes = num_boxes
         self.min_servo, self.max_servo = min_servo, max_servo
         if num_boxes < 1:
             raise ValueError("You must init at least one box")
+        pi.set_mode(pin, pigpio.OUTPUT)
     def calc_pulse(self, box, verbose=True):#box is from 0 to self.num_boxes-1
         angle = 180/self.num_boxes * (box+0.5)
         pulse = (angle/180*(self.max_servo - self.min_servo)) + self.min_servo
         if not verbose:
             print("Angle:", angle, "\nPulselength:", pulse)
         return pulse
+    def goto_angle(self, angle):
+        pulse = calc_pulse(angle)
+        
