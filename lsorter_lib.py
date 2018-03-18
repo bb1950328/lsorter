c0d16c0c7bc953efab945916dcc3d1eff3a93e0b
bb1950328 <bb1950328@gmail.com>
2018-03-18 15:04:09 +0100

    Update captured1521381452.61.png, captured1521381453.99.png, and 15 more files...

--- lsorter_lib.py
+++ lsorter_lib.py
@@ -44,10 +44,17 @@ class slideturner():
         if not verbose:
             print("Angle:", angle, "\tPulselength:", pulse)
         return pulse
-    def goto_box(self, box, verbose=True):
+    def goto_box(self, box, really_delay, verbose=True, delay=0):
+        if delay:
+            self.thread = Thread(target=self.goto_box, args=(box, delay))
+            self.thread.start()
+            return
+        if really_delay:
+            time.sleep(really_delay)
         self.box = box
         pulse = self.calc_pulse(box, verbose=verbose)
         if on_pi:
+            
             pi.set_servo_pulsewidth(self.pin, pulse)
             print("setservo")
         else:
@@ -140,22 +147,22 @@ class Coordinator():
         self.so = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.si = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.si.bind(("localhost", 54321))
-        self.belt = unipolarstepper([17, 27, 22, 10])
+        #self.belt = unipolarstepper([17, 27, 22, 10])
         self.slide = slideturner(3)
     def start(self):
-        self.belt.drive_rpm(0)
+        #self.belt.drive_rpm(0)
         while True:
             self.so.sendto(b"wait", ("localhost", 12345))
             print("sent request.")
-            data = self.si.recv(1024)
-            if data.decode() == "Red":
-                self.slide.goto_box(0, verbose=False)
-            elif data.decode() == "Green":
-                self.slide.goto_box(1, verbose=False)
-            elif data.decode() == "Blue":
-                self.slide.goto_box(2, verbose=False)
-            
-            print(data)
+            color = self.si.recv(1024)
+            delay = float(self.si.recv(1024))
+            if color.decode() == "Red":
+                self.slide.goto_box(0, False, verbose=False, delay=delay)
+            elif color.decode() == "Green":
+                self.slide.goto_box(1, False, verbose=False, delay=delay)
+            elif color.decode() == "Blue":
+                self.slide.goto_box(2, False, verbose=False, delay=delay)
+            print(color, delay)
 
 print("Initalised classes.")
 Coordinator().start()
