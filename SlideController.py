36ce603192e88d79e3154046e2491f968d37a9e0
bb1950328 <bb1950328@gmail.com>
2018-05-20 11:33:52 +0200

    Sorting the six colors without errors

--- SlideController.py
+++ SlideController.py
@@ -12,8 +12,10 @@ import pigpio
 aparser = ArgumentParser(description = "script to control a servo via UDP")
 aparser.add_argument("-p", "--servo-pin", dest = "pin", metavar = "Servo pin", type = int, help = "Which BCM GPIO pin device to use (Default 4)", default = 4)
 aparser.add_argument("-i", "--ip", dest = "ip", metavar = "IP", type = str, help = "IP Address of the controller", default = "127.0.0.1")
-aparser.add_argument("-r", "--rx-port", dest = "rxport", metavar = "RX Port", type = int, help = "Port to receive commands (Default 50004)", default = 50004)
+aparser.add_argument("-r", "--rx-port", dest = "rxport", metavar = "RX Port", type = int, help = "Port to receive commands (Default 50006)", default = 50006)
 args = aparser.parse_args()
+
+on_pi = True
 
 try:
     pi = pigpio.pi()
@@ -36,7 +38,7 @@ class slideturner():
     def calc_pulse(self, box, verbose=True):#box is from 0 to self.num_boxes-1
         angle = 180/self.num_boxes * (box+0.5)
         pulse = (angle/180*(self.max_servo - self.min_servo)) + self.min_servo
-        if not verbose:
+        if verbose:
             print("Angle:", angle, "\tPulselength:", pulse)
         return pulse
     def goto_box(self, box, really_delay, verbose=True, delay=0):
@@ -49,21 +51,27 @@ class slideturner():
         self.box = box
         pulse = self.calc_pulse(box, verbose=verbose)
         if on_pi:
-            
             pi.set_servo_pulsewidth(self.pin, pulse)
             print("setservo")
         else:
             print("[would set servo to {}ms if there's a servo.]".format(pulse))
 
 slide = slideturner(args.pin)
-self.slide.goto_box(0, False, verbose=False, delay=0)
+slide.goto_box(0, False, verbose=False, delay=0)
 rxsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+print(args.ip, args.rxport)
 rxsock.bind((args.ip, args.rxport))
 r = ""
 try:
 	while True:
-		r = rxsock.recv(1024)
-		if r == "num_slides":
-			pass
-		#TODO: if r=="kjgrewlkgdrhnsög"
-
+		r, addr = rxsock.recvfrom(1024)
+		r = r.decode()
+		print(r)
+		rs = r.split()
+		if "num_boxes" in r:
+			slide.num_boxes = int(rs[1])
+		elif "set" in r:
+			slide.goto_box(int(rs[1]), (float(rs[1]) if len(rs)>2 else 0))
+except KeyboardInterrupt:
+	rxsock.close()
+	sys.exit()
